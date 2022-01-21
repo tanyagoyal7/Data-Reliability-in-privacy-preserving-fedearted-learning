adc5d1f7312de6d986a5e734ec985476bbb2a2f3
vidhi-aggarwal <46401624+vidhi-aggarwal@users.noreply.github.com>
2022-01-21 22:26:53 +0530

    Update plot_code.m

--- plot_code.m
+++ plot_code.m
@@ -2,12 +2,16 @@ clc;
 clear all;
 close all;
 
-load('noise_surface_data_time30_mean_malicious5.mat');
+load('noise_surface_data_time30_mean_malicious5_.mat');
 load('noise_surface_data.mat');
 
-
-figure;
-
-plot3(X,Y,noise_map_,'o');
-hold on;
-plot3(X,Y,malicious_points,'.');
+for t=1:30
+   
+    figure;
+    noisemap=reshape(noise_map_(t,:,:),151,161);
+    plot3(X,Y,noisemap,'o');
+    hold on;
+ 
+    malpoints=reshape(malicious_points(t,:,:),151,161);
+    plot3(X,Y,malpoints,'.');
+end
